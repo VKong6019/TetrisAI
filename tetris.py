d9a93c1442b440261f9eb3c748f7f06ceb91e0c5
Nicole Danuwidjaja <nicoledanuwidjaja@macbook-pro.myfiosgateway.com>
2021-11-23 04:53:10 -0500

    add more starter utils and tweaks

--- tetris.py
+++ tetris.py
@@ -1,5 +1,10 @@
+from collections import defaultdict
+
 import pygame
 import random
+import copy
+import numpy as np
+
 
 colors = [
     (0, 0, 0),
@@ -11,7 +16,10 @@ colors = [
     (180, 34, 122),
 ]
 
+line_scores = { 0:0, 1: 40, 2: 100, 3: 300, 4: 1200 }
 
+
+# tetrominoes represented as 4x4 matrix
 class Figure:
     x = 0
     y = 0
@@ -26,9 +34,13 @@ class Figure:
         [[1, 2, 5, 6]],
     ]
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, block_type, color, rotation):
         self.x = x
         self.y = y
+        if block_type is not None:
+            self.type = block_type
+            self.color = color
+            self.rotation = rotation
         self.type = random.randint(0, len(self.figures) - 1)
         self.color = random.randint(1, len(colors) - 1)
         self.rotation = 0
@@ -41,31 +53,58 @@ class Figure:
 
 
 class Tetris:
-    level = 2
-    score = 0
-    state = "start"
-    field = []
     height = 0
     width = 0
     x = 100
     y = 60
     zoom = 20
-    figure = None
 
     def __init__(self, height, width):
         self.height = height
         self.width = width
         self.field = []
+        self.figure = None
         self.score = 0
         self.state = "start"
-        for i in range(height):
+        self.best_score = float('inf')
+        self.optimal_move = None
+        self.optimal_rotation = None
+
+        for _ in range(height):
             new_line = []
-            for j in range(width):
+            for _ in range(width):
                 new_line.append(0)
             self.field.append(new_line)
 
+    # Generates new piece and calculates its optimal move
+    def freeze(self):
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in self.figure.image():
+                    self.field[i + self.figure.y][j + self.figure.x] = self.figure.color
+        self.break_lines()
+        self.new_figure()
+        if self.intersects():
+            self.state = "gameover"
+
+    # Checks if any lines can be cleared
+    def break_lines(self):
+        lines = 0
+        for i in range(1, self.height):
+            zeros = 0
+            for j in range(self.width):
+                if self.field[i][j] == 0:
+                    zeros += 1
+            if zeros == 0:
+                lines += 1
+                for i1 in range(i, 1, -1):
+                    for j in range(self.width):
+                        self.field[i1][j] = self.field[i1 - 1][j]
+        self.score += line_scores[lines]
+
+    # Generates next Tetris piece and finds its best move
     def new_figure(self):
-        self.figure = Figure(3, 0)
+        self.figure = Figure(3, 0, None, None, None)
 
     def intersects(self):
         intersection = False
@@ -79,19 +118,20 @@ class Tetris:
                         intersection = True
         return intersection
 
-    def break_lines(self):
-        lines = 0
-        for i in range(1, self.height):
-            zeros = 0
-            for j in range(self.width):
-                if self.field[i][j] == 0:
-                    zeros += 1
-            if zeros == 0:
-                lines += 1
-                for i1 in range(i, 1, -1):
-                    for j in range(self.width):
-                        self.field[i1][j] = self.field[i1 - 1][j]
-        self.score += lines ** 2
+    def intersects_with_figure(self, figure, dx, dy):
+        intersection = False
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in figure.image():
+                    if i + dy + figure.y > self.height - 1 or \
+                            j + dx + figure.x > self.width - 1 or \
+                            j + dx + figure.x < 0 or \
+                            self.field[i + dy + figure.y][j + dx + figure.x] > 0:
+                        intersection = True
+        return intersection
+
+
+    ### MOVEMENT ###
 
     def go_space(self):
         while not self.intersects():
@@ -105,27 +145,38 @@ class Tetris:
             self.figure.y -= 1
             self.freeze()
 
-    def freeze(self):
-        for i in range(4):
-            for j in range(4):
-                if i * 4 + j in self.figure.image():
-                    self.field[i + self.figure.y][j + self.figure.x] = self.figure.color
-        self.break_lines()
-        self.new_figure()
-        if self.intersects():
-            self.state = "gameover"
-
     def go_side(self, dx):
         old_x = self.figure.x
         self.figure.x += dx
         if self.intersects():
             self.figure.x = old_x
 
+    # go all the way to the right, then all the way down, then all the way to the left, and then down
+    def go_default(self):
+        while not self.intersects():
+            self.figure.x += 1
+        self.figure.x -= 1
+        while not self.intersects():
+            self.figure.y += 1
+        self.figure.y -= 1
+        while not self.intersects():
+            self.figure.x -= 1
+        self.figure.x += 1
+        while not self.intersects():
+            self.figure.y += 1
+        self.figure.y -= 1
+        print("DEFAULT")
+        self.freeze()
+
     def rotate(self):
         old_rotation = self.figure.rotation
         self.figure.rotate()
         if self.intersects():
             self.figure.rotation = old_rotation
+
+    def get_string_field(self):
+        print(np.array(self.field))
+
 
 
 # Initialize the game engine
@@ -136,7 +187,9 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
 
-size = (400, 500)
+WIDTH = 400
+HEIGHT = 500
+size = (WIDTH, HEIGHT)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Tetris")
@@ -146,9 +199,8 @@ done = False
 clock = pygame.time.Clock()
 fps = 25
 game = Tetris(20, 10)
-counter = 0
 
-pressing_down = False
+action_seq = []
 
 while not done:
     ######################################################################
@@ -156,13 +208,6 @@ while not done:
 
     if game.figure is None:
         game.new_figure()
-    counter += 1
-    if counter > 100000:
-        counter = 0
-
-    if counter % (fps // game.level // 2) == 0 or pressing_down:
-        if game.state == "start":
-            game.go_down()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -171,7 +216,7 @@ while not done:
             if event.key == pygame.K_UP:
                 game.rotate()
             if event.key == pygame.K_DOWN:
-                pressing_down = True
+                game.go_down()
             if event.key == pygame.K_LEFT:
                 game.go_side(-1)
             if event.key == pygame.K_RIGHT:
@@ -180,10 +225,25 @@ while not done:
                 game.go_space()
             if event.key == pygame.K_ESCAPE:
                 game.__init__(20, 10)
-
-    if event.type == pygame.KEYUP:
-            if event.key == pygame.K_DOWN:
-                pressing_down = False
+    
+        if game.state != "gameover":
+            print(action_seq)
+            if len(action_seq) > 0:
+                action = action_seq.pop()
+                if action == "right":
+                    game.go_side(1)
+                elif action == "left":
+                    game.go_side(-1)
+                elif action == "down":
+                    game.go_down()
+                elif action == "space":
+                    game.go_space()
+                elif action == "rotate":
+                    game.rotate()
+                else:
+                    game.go_default()
+            else:
+                game.get_string_field()
 
     # end of revision
     ######################################################################
@@ -195,7 +255,7 @@ while not done:
             pygame.draw.rect(screen, GRAY, [game.x + game.zoom * j, game.y + game.zoom * i, game.zoom, game.zoom], 1)
             if game.field[i][j] > 0:
                 pygame.draw.rect(screen, colors[game.field[i][j]],
-                                 [game.x + game.zoom * j + 1, game.y + game.zoom * i + 1, game.zoom - 2, game.zoom - 1])
+                                [game.x + game.zoom * j + 1, game.y + game.zoom * i + 1, game.zoom - 2, game.zoom - 1])
 
     if game.figure is not None:
         for i in range(4):
@@ -203,20 +263,18 @@ while not done:
                 p = i * 4 + j
                 if p in game.figure.image():
                     pygame.draw.rect(screen, colors[game.figure.color],
-                                     [game.x + game.zoom * (j + game.figure.x) + 1,
-                                      game.y + game.zoom * (i + game.figure.y) + 1,
-                                      game.zoom - 2, game.zoom - 2])
+                                    [game.x + game.zoom * (j + game.figure.x) + 1,
+                                    game.y + game.zoom * (i + game.figure.y) + 1,
+                                    game.zoom - 2, game.zoom - 2])
 
-    font = pygame.font.SysFont('Calibri', 25, True, False)
-    font1 = pygame.font.SysFont('Calibri', 65, True, False)
+    font = pygame.font.SysFont('comicsans', 25, True, False)
+    font1 = pygame.font.SysFont('comicsans', 70, True, False)
     text = font.render("Score: " + str(game.score), True, BLACK)
-    text_game_over = font1.render("Game Over", True, (255, 125, 0))
-    text_game_over1 = font1.render("Press ESC", True, (255, 215, 0))
+    text_game_over = font1.render("Game Over!", True, (250, 125, 125))
 
-    screen.blit(text, [0, 0])
+    screen.blit(text, [10, 10])
     if game.state == "gameover":
-        screen.blit(text_game_over, [20, 200])
-        screen.blit(text_game_over1, [25, 265])
+        screen.blit(text_game_over, [50, 200])
 
     pygame.display.flip()
     clock.tick(fps)
